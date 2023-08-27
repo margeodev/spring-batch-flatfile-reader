646811e961210bc761deca54631d89f7720bf241
Marcio <margeodev@gmail.com>
2023-08-27 19:42:54 -0300

    Initial commit.

---
+++ src/main/java/com/springbatch/flatfile/FlatFileReaderApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.flatfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatFileReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatFileReaderApplication.class, args);
+	}
+
+}
