a4a6347ebaf21b0865df6b66e55c478097974e71
Marcio <margeodev@gmail.com>
2023-08-27 20:16:20 -0300

    Creation of Client.class and clients.txt file.

---
+++ src/main/java/com/springbatch/flatfile/model/Client.java
@@ -0,0 +1,11 @@
+package com.springbatch.flatfile.model;
+
+import lombok.Data;
+
+@Data
+public class Client {
+    private String name;
+    private String lastName;
+    private Integer age;
+    private String email;
+}
